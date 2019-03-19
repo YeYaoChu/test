c297255c894759847707d965802a0e2478bd75bd
Dell <2457017831@qq.com>
2019-03-19 16:57:38 +0800

    第一次提交

---
+++ src/main/java/com/hr/springboot5/dao/StudentMapper.java
@@ -0,0 +1,16 @@
+package com.hr.springboot5.dao;
+
+import com.hr.springboot5.entity.Student;
+import java.util.List;
+
+public interface StudentMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Student record);
+
+    Student selectByPrimaryKey(Integer id);
+
+    List<Student> selectAll();
+
+    int updateByPrimaryKey(Student record);
+}
